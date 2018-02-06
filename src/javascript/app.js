5bafdd37547203d9dca399dbf06e09cefd5fa52a
leaztanda <leaztanda@gmail.com>
2018-02-06 23:04:47 +0100

    show list categories with description js

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,12 +3,36 @@ var niceModule = require('./modules/niceModule')
 var dev = require('./dev') 
 var utils = require('./modules/utils-test')
 
+// utils.findAllIn('students').then(function(data) {
+//     console.log(data);
+// });
 
 
-utils.findAllIn('students').then(function(data) {
-    console.log(data);
-});
-
+//Show category list in homepage
+var categoriesList = document.getElementById('categories-list');
+if (categoriesList) {
+    utils.findAllIn('categories').then(function(data) {
+        for (let i = 0; i < data.length; i++) {
+            let categoryName = data[i].name,
+                longCategoryTitle = categoryName.length > 15 ? 'projects__category-title-long' : '';
+                console.log(categoryName.length);
+            var categoryHTML = 
+            `<section class="projects__category">
+                <div class="projects__category-title ${longCategoryTitle}">
+                        <h2>${categoryName}</h2>
+                    </div>
+                    <div class="projects__category-content">
+                    <div class="projects__category-thumbnail"></div>
+                        <div class="projects__category-description">
+                            <p>${data[i].description}</p>
+                            <span>Next</span>
+                        </div>
+                    </div>
+            </section>`;
+            categoriesList.innerHTML += categoryHTML;
+        }
+    });
+}
 
 // utils.findOneByIn('students', 'firstname', 'Robin').then(function(data){
 //     console.log(data)
--- src/javascript/dev.js
+++ src/javascript/dev.js
@@ -1,26 +1,26 @@
-var utils = require('./modules/utils-test');
+// var utils = require('./modules/utils-test');
 
-var body = document.querySelector('body');
+// var body = document.querySelector('body');
 
-utils.findAllIn('lea').then(function(categories) {
-    for (let i = 0; i < 1; i++) {
-        var table = document.createElement('table');
-        document.body.appendChild(table);
-        table.style.border = '1px solid red';
-        table.innerHTML = '<caption>'+categories[i].name+'</caption><thead><tr><th>Nom projet</th><th>Chemin fichier screens</th><th>Chemin fichier projet</th><th>Binomes</th></tr></thead><tbody></tbody>';
-        var tbody = table.querySelector('tbody'),
-            projectsList = categories[i].projectsList;
+// utils.findAllIn('lea').then(function(categories) {
+//     for (let i = 0; i < 1; i++) {
+//         var table = document.createElement('table');
+//         document.body.appendChild(table);
+//         table.style.border = '1px solid red';
+//         table.innerHTML = '<caption>'+categories[i].name+'</caption><thead><tr><th>Nom projet</th><th>Chemin fichier screens</th><th>Chemin fichier projet</th><th>Binomes</th></tr></thead><tbody></tbody>';
+//         var tbody = table.querySelector('tbody'),
+//             projectsList = categories[i].projectsList;
 
-        for (let k = 0; k < projectsList.length ; k++) {
-            utils.findStudentsByProject(projectsList[k].id.toString()).then(function(students) {
-                var studentsNames = '';
-                for (let j = 0; j < students.length; j++) {
-                    studentsNames += students[j].lastname + ' ' + students[j].firstname + ' - ';
-                    if (j == students.length - 1){
-                        tbody.innerHTML += '<tr><td>'+projectsList[k].name+' </td><td>'+categories[i].ident+'/'+projectsList[k].id+'_'+projectsList[k].ident+'/screen</td><td>'+categories[i].ident+'/'+projectsList[k].id+'_'+projectsList[k].ident+'/projet</td><td>'+studentsNames+'</td></tr>';
-                    }
-                }
-            })
-        }
-    }
-});
+//         for (let k = 0; k < projectsList.length ; k++) {
+//             utils.findStudentsByProject(projectsList[k].id.toString()).then(function(students) {
+//                 var studentsNames = '';
+//                 for (let j = 0; j < students.length; j++) {
+//                     studentsNames += students[j].lastname + ' ' + students[j].firstname + ' - ';
+//                     if (j == students.length - 1){
+//                         tbody.innerHTML += '<tr><td>'+projectsList[k].name+' </td><td>'+categories[i].ident+'/'+projectsList[k].id+'_'+projectsList[k].ident+'/screen</td><td>'+categories[i].ident+'/'+projectsList[k].id+'_'+projectsList[k].ident+'/projet</td><td>'+studentsNames+'</td></tr>';
+//                     }
+//                 }
+//             })
+//         }
+//     }
+// });
