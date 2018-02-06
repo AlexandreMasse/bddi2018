ef41ad1cb55b3123553766173fa147fa43e626ac
leaztanda <leaztanda@gmail.com>
2018-02-06 01:21:36 +0100

    add dev file path list

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,17 +1,18 @@
 //var $ = require('jquery');
 var niceModule = require('./modules/niceModule')
 var utils = require('./modules/utils')
+var dev = require('./dev')
 
 utils.findAllIn('students');
 
 utils.findOneByIn('students', 'firstname', 'Robin', function(data){
-    console.log(data)
+    // console.log(data)
 });
 
 utils.findStudentsByProject('1', function(data) {
-    console.log(data);
+    // console.log(data);
 })
 
 utils.findAllProjectsByCategory('0', function(data){
-    console.log(data)
+    // console.log(data)
 })
