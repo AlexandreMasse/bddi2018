81e0c4f4ad24e4241542246b47a1bc511fdb844c
Circé <circegrand@gmail.com>
2018-02-08 19:43:55 +0100

    Change header css

---
+++ src/javascript/modules/animation.js
@@ -0,0 +1,13 @@
+var menu_open = document.querySelector(".header_menu-open");
+var menu_close = document.querySelector(".header_menu-close");
+var body = document.querySelector("body");
+
+menu_open.addEventListener("click", function() {
+  body.classList.add("menu-active");
+  menu_open.classList.add("hidden");
+  menu_close.classList.remove("hidden");
+});
+
+menu_close.addEventListener("click", function() {
+  body.classList.remove("menu-active");
+});
