f34479de9e9445d55b3a1625532edd5542b697f2
leaztanda <leaztanda@gmail.com>
2018-02-24 15:02:22 +0100

    edit about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,5 +4,8 @@
     <p>Developed by <a href="#">Circé Grand</a>, <a href="#">Alexandre Massé</a> & <a href="#">Léa Tanda</a></p>
     <p>Designed by <a href="#">Elisa Dubois</a></p>
     <p>Characters designed by <a href="#">Matthieu Pajot</a></p>
+    <p>Bachelor Designer & Developpeur Interactif </p>
+    <p>Promo 2018</p>
+    <p>Gobelins, l'École de l'Image</p>
   </div>
 </template>
