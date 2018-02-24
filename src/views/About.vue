283ed3a8edde0b03372409c5bcf2f3dcd60dfcc4
leaztanda <leaztanda@gmail.com>
2018-02-24 14:58:56 +0100

    edit about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Developed by <a href="#">Circé Grand</a>, <a href="#">Alexandre Massé</a> & <a href="#">Léa Tanda</a></p>
+    <p>Designed by <a href="#">Elisa Dubois</a></p>
+    <p>Characters designed by <a href="#">Matthieu Pajot</a></p>
   </div>
 </template>
