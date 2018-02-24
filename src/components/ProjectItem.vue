ebdb37a07ecc336ca5ed1136ffa69ff5f140d3d9
leaztanda <leaztanda@gmail.com>
2018-02-24 19:31:25 +0100

    list projects in category view

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,9 +1,89 @@
 <template>
-  <p>Je suis un item projet de la page catégorie, multipliez- moi !</p>
+  <div class="projects__list-item">
+    <div class="project__thumbnail">
+        <div class="project__thumbnail-img" style="background-image: url('/img/thumbnail-0.1994789b.jpg')">
+        </div>
+    </div>
+    <h2>{{project.name}}</h2>
+    <p>
+      <span v-for="student in studentListOutput" :key="student.id">{{student[0].firstname}} <em>({{student[0].option}})</em> - </span>
+    </p>
+  </div>
 </template>
 
 <script>
+  import studentsList from '@/data/students.json'
+
   export default {
-    name: 'projectItem'
+    name: 'projectItem',
+    props: ['data'],
+    data () {
+      return {
+        project: this.data
+      }
+    },
+    computed: {
+      studentListOutput () {
+        var students = []
+        for (let i = 0; i < this.project.studentsList.length; i++) {
+          let student = this.project.studentsList[i]
+          students.push(studentsList.filter(studentList => student === studentList.id))
+          if (i === this.project.studentsList.length - 1) {
+            return students
+          }
+        }
+      }
+    }
   }
 </script>
+
+<style lang="scss" scoped>
+  .projects__list-item {
+    cursor: pointer;
+    width: 300px;
+    height: 350px;
+    float: left;
+
+    &:nth-of-type(3n+2) {
+      margin: 0 20px;
+    }
+
+    .project__thumbnail {
+      width: 300px;
+      height: 200px;
+      overflow: hidden;
+
+      &-img {
+        width: 300px;
+        height: 200px;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        // background-image: url('../images/placeholder-thumbnail.png');
+        transition: all 0.7s ease;
+      }
+    }
+
+    h2 {
+      // font-family: $font-walsheim-regular;
+      margin: 15px 2px 10px 2px;
+      font-size: 20px;
+      line-height: 25px;
+    }
+
+    p {
+      // font-family: $font-aileron-light;
+      font-size: 12px;
+      line-height: 20px;
+      opacity: 0.8;
+    }
+
+    &:hover {
+      .project__thumbnail {
+        &-img {
+          transform: scale(1.1);
+        }
+      }
+    }
+  }
+</style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -2,19 +2,32 @@
   <div class="category">
    <h1>{{text}}</h1>
    <h2>Ident : {{categoryIdent}}</h2>
-   <project-item></project-item>
+   <project-item v-for="project in projects" :key="project.id" :data=project></project-item>
   </div>
 </template>
 
 <script>
   import projectItem from '@/components/ProjectItem.vue'
+  import categories from '@/data/categories.json' // To delete soon
+
   export default {
     name: 'category',
     components: {projectItem},
     data () {
       return {
         text: 'Category',
-        categoryIdent : this.$route.params.ident
+        categoryIdent: this.$route.params.ident
+      }
+    },
+    computed: {
+      projects () {
+        for (let i = 0; i < categories.length; i++) {
+          if (categories[i].ident === this.categoryIdent) {
+            return categories[i].projectsList
+          } else if (i === categories.length || this.categoryIdent === 'default') {
+            return categories[0].projectsList
+          }
+        }
       }
     }
   }
